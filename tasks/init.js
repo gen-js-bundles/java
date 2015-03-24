ec38d24ea7a759bcf3fbf701ebc8f38a5f554529
ludo1026 <ludo1026@free.fr>
2015-03-24 11:42:50 +0100

    facets

--- tasks/init.js
+++ tasks/init.js
@@ -10,156 +10,219 @@ module.exports = {
 
     var dependenciesChoices = [
       {
-	name: "Test - JUnit - http://junit.org/",
-	value: {
-          groupId: "junit",
-          artifactId: "junit",
-          version: "4.12",
-          scope: "test"
+        name: "Test - JUnit - http://junit.org/",
+        value: {
+          dependency: {
+            groupId: "junit",
+            artifactId: "junit",
+            version: "4.12",
+            scope: "test"
+          }
         },
         checked: true
       },
       {
-	name: "Test - Mockito - http://www.mockito.org/",
-	value: {
-          groupId: "org.mockito",
-          artifactId: "mockito-all",
-          version: "1.10.19",
-          scope: "test"
+        name: "Test - Mockito - http://www.mockito.org/",
+        value: {
+          dependency: {
+            groupId: "org.mockito",
+            artifactId: "mockito-all",
+            version: "1.10.19",
+            scope: "test"
+          }
         }
       },{
       	name: "Test - Spring - https://github.com/spring-projects/spring-framework",
-	value: {
-          groupId: "org.springframework",
-          artifactId: "spring-test",
-          version: "4.1.5.RELEASE",
-          scope: "test"
+        value: {
+          dependency: {
+            groupId: "org.springframework",
+            artifactId: "spring-test",
+            version: "4.1.5.RELEASE",
+            scope: "test"
+          },
+          facets: [
+            'test'
+          ]
         }
       },{
         name: "MongoDB - MongoDB Java Driver - http://docs.mongodb.org/ecosystem/drivers/java",
-	  value: {
-          groupId: "org.mongodb",
-          artifactId: "mongo-java-driver",
-          version: "2.13.0"
+        value: {
+          dependency: {
+            groupId: "org.mongodb",
+            artifactId: "mongo-java-driver",
+            version: "2.13.0"
+          },
+          facets: [
+            'mongodb'
+          ]
         }
       },{
         name: "MongoDB - Morphia - https://github.com/mongodb/morphia",
         value: {
-          groupId: "org.mongodb.morphia",
-          artifactId: "morphia",
-          version: "0.110"
+          dependency: {
+            groupId: "org.mongodb.morphia",
+            artifactId: "morphia",
+            version: "0.110"
+          },
+          facets: [
+            'mongodb'
+          ]
         }
       },{
         name: "MongoDB - Jongo - http://jongo.org",
         value: {
-          groupId: "org.jongo",
-          artifactId: "jongo",
-          version: "1.1"
+          dependency: {
+            groupId: "org.jongo",
+            artifactId: "jongo",
+            version: "1.1"
+          },
+          facets: [
+            'mongodb'
+          ]
         }
       },{
         name: "MongoDB - MongoJack - http://mongojack.org", 
         value: {
-          groupId: "org.mongojack",
-          artifactId: "mongojack",
-          version: "2.3.0"
+          dependency: {
+            groupId: "org.mongojack",
+            artifactId: "mongojack",
+            version: "2.3.0"
+          },
+          facets: [
+            'mongodb'
+          ]
         }
       },{
         name: "MongoDB - Spring Data MongoDB - http://projects.spring.io/spring-data-mongodb", 
         value: {
-          groupId: "org.springframework.data",
-          artifactId: "spring-data-mongodb",
-          version: "1.6.2.RELEASE"
+          dependency: {
+            groupId: "org.springframework.data",
+            artifactId: "spring-data-mongodb",
+            version: "1.6.2.RELEASE"
+          },
+          facets: [
+            'mongodb'
+          ]
         }
       },{
         name: "Data - JDBI - SQL convenience library - http://jdbi.org", 
         value: {
-          groupId: "org.jdbi",
-          artifactId: "jdbi",
-          version: "2.59"
+          dependency: {
+            groupId: "org.jdbi",
+            artifactId: "jdbi",
+            version: "2.59"
+          },
+          facets: [
+            'jdbi'
+          ]
         }
       },{
         name: "Database driver - MySQL - http://dev.mysql.com/usingmysql/java", 
         value: {
-          groupId: "mysql",
-          artifactId: "mysql-connector-java",
-          version: "5.1.34"
+          dependency: {
+            groupId: "mysql",
+            artifactId: "mysql-connector-java",
+            version: "5.1.34"
+          }
         }
       },{
         name: "Database driver - PostgreSQL - http://jdbc.postgresql.org", 
         value: {
-          groupId: "org.postgresql",
-          artifactId: "postgresql",
-          version: "9.4-1201-jdbc41"
+          dependency: {
+            groupId: "org.postgresql",
+            artifactId: "postgresql",
+            version: "9.4-1201-jdbc41"
+          }
         }
       },{
         name: "Database driver - H2 - http://www.h2database.com", 
         value: {
-          groupId: "com.h2database",
-          artifactId: "h2",
-          version: "1.4.185"
+          dependency: {
+            groupId: "com.h2database",
+            artifactId: "h2",
+            version: "1.4.185"
+          }
         }
       },{
         name: "Database driver - HSQLDB - http://dev.mysql.com/usingmysql/java/", 
         value: {
-          groupId: "org.postgresql",
-          artifactId: "postgresql",
-          version: "9.4-1201-jdbc41"
+          dependency: {
+            groupId: "org.postgresql",
+            artifactId: "postgresql",
+            version: "9.4-1201-jdbc41"
+          }
         }
       },{
         name: "Database driver - Oracle - ojdbc14 - http://www.oracle.com/technology/software/tech/java/sqlj_jdbc", 
         value: {
-          groupId: "com.oracle",
-          artifactId: "ojdbc14",
-          version: "10.2.0.4.0"
+          dependency: {
+            groupId: "com.oracle",
+            artifactId: "ojdbc14",
+            version: "10.2.0.4.0"
+          }
         }
       },{
         name: "Database driver - Elasticsearch - http://www.elasticsearch.org", 
         value: {
-          groupId: "org.elasticsearch",
-          artifactId: "elasticsearch",
-          version: "1.4.4"
+          dependency: {
+            groupId: "org.elasticsearch",
+            artifactId: "elasticsearch",
+            version: "1.4.4"
+          }
         }
       },{
         name: "Database driver - Redis - Jedis - https://github.com/xetorthio/jedis", 
         value: {
-          groupId: "redis.clients",
-          artifactId: "jedis",
-          version: "2.6.2"
+          dependency: {
+            groupId: "redis.clients",
+            artifactId: "jedis",
+            version: "2.6.2"
+          }
         }
       },{
         name: "Database driver - RabbitMQ - http://www.rabbitmq.com", 
         value: {
-          groupId: "com.rabbitmq",
-          artifactId: "amqp-client",
-          version: "3.4.4"
+          dependency: {
+            groupId: "com.rabbitmq",
+            artifactId: "amqp-client",
+            version: "3.4.4"
+          }
         }
       },{
         name: "Web - webjars - AngularJS - http://jdbi.org", 
         value: {
-          groupId: "org.webjars",
-          artifactId: "angularjs",
-          version: "1.3.14"
+          dependency: {
+            groupId: "org.webjars",
+            artifactId: "angularjs",
+            version: "1.3.14"
+          }
         }
       },{
         name: "Jsoup - http://jsoup.org/", 
         value: {
-	  groupId: "org.jsoup",
-	  artifactId: "jsoup",
-	  version: "1.8.1"
+          dependency: {
+            groupId: "org.jsoup",
+            artifactId: "jsoup",
+            version: "1.8.1"
+          }
         }
       },{
         name: "Apache HTTPComponents - HTTPClient - http://hc.apache.org/httpcomponents-client-ga/", 
         value: {
-	  groupId: "org.apache.httpcomponents",
-	  artifactId: "httpclient",
-	  version: "4.4"
+          dependency: {
+            groupId: "org.apache.httpcomponents",
+            artifactId: "httpclient",
+            version: "4.4"
+          }
         }
       },{
         name: "Apache HTTPComponents - HTTPCore - http://hc.apache.org/httpcomponents-core-ga/", 
         value: {
-	  groupId: "org.apache.httpcomponents",
-	  artifactId: "httpcore",
-	  version: "4.4"
+          dependency: {
+            groupId: "org.apache.httpcomponents",
+            artifactId: "httpcore",
+            version: "4.4"
+          }
         }
       }
     ];
@@ -274,11 +337,28 @@ module.exports = {
       }
       data.global.build.tool = answers.buildTool;
 
+      data.global.facets = [];
+      var facets = {};
+      for(var i=0; i<answers.dependenciesSelected.length; i++) {
+        var dependencySelected = answers.dependenciesSelected[i];
+        if(dependencySelected.facets != null) {
+          for(var j=0; j<dependencySelected.facets.length; j++) {
+            var facet = dependencySelected.facets[j];
+            facets[facet] = true;
+          }
+        }
+      }
+      data.global.facets = facets;
+
       gfile.writeYaml(path.join(process.cwd(),'Genjsfile.yml'), data);
 
       var data = {
-        dependencies: answers.dependenciesSelected
+        dependencies: []
       };
+      for(var i=0; i<answers.dependenciesSelected.length; i++) {
+        var dependencySelected = answers.dependenciesSelected[i];
+        data.dependencies.push(dependencySelected.dependency);
+      }
 
       gfile.writeYaml(path.join(process.cwd(),'model','config.@build.yml'), data);
 
