48631cc2d4f700ea5a07e2224f6ae3e3f3807de9
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-27 09:20:45 +0100

    rest

---
+++ templates/{src}/PPP/@rest/web/rest/[name_A]Resource.java
@@ -0,0 +1,20 @@
+<% var rest = current.tags.rest %>
+public <%=current.name.A()%>Resource {
+
+
+<% each(rest.paths, function(pathInfo,path) {%>
+  <% each(pathInfo.methods, function(method, methodHttp) {%>
+    /* <%=methodHttp%> <%=method.name%> */
+    @RequestMapping(value = "<%=path%>",
+        method = RequestMethod.<%=methodHttp%>,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public <%=method.return%> <%=method.name%>() {
+
+    }
+
+
+  <%})%>
+<%})%>
+}
+cd
